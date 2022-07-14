ff172bad5553c4a76cd6d990c9bde0e4e39d0f6e
0xFrian <brianfong22@gmail.com>
2022-07-14 16:49:44 -0700

    :bulb: Added comments to functions

--- scripts/web-scraping.js
+++ scripts/web-scraping.js
@@ -1,23 +1,29 @@
-// Scraping websites
-
-// ===== Import packages =====
 const puppeteer = require("puppeteer"); 
 
-// ===== Helper function: filter raw text content =====
-function filterText(articleText) {
-    let copyText = articleText.join("\n").split("\n");
-    copyText = copyText.map(item => item.trim());
-    let filteredText = [];
-    for (let i = 0; i < copyText.length; i++) {
-        let item = copyText[i];
-        if (item.length > 0) {
-            filteredText.push(item); 
+/**
+    * Parses through string array, separating each entry with 1 newline-character ("\n")
+    *
+    * @param {array} textArr : String array containing text content (raw/unfiltered)
+    * @return {array} textArr_filtered : String array containing filtered text content
+    */
+function filterText(textArr) {
+    let textArr_copy = textArr.map(item => item.trim()); // Remove leading/trailing whitespace
+    let textArr_filt = [];
+    for (let i = 0; i < textArr_copy.length; i++) {
+        let text = textArr_copy[i];
+        if (text.length > 0) {
+            textArr_filt.push(text); 
         }
     }
-    return filteredText;
+    return textArr_filt;
 }
 
-// ===== Helper function: print list of content =====
+/**
+    * Parses through JSON object representing an article, console-logging its properties: title and content
+    *
+    * @param {object} content : Object containing title and content of an article
+    * @return {none}
+    */
 function printContent(content) {
     console.log("Title: ");
     console.log(content.title.join() + "\n");
@@ -25,14 +31,18 @@ function printContent(content) {
     console.log(content.text.join("\n\n"));
 }
 
-// ===== Main function: scrape article for text content =====
+/**
+    * Scrapes textual content from an article given its website URL
+    *
+    * @param {string} websiteURL : String representing the article's website URL
+    * @return {object} content : Object containing title and content of an article
+    */
 async function scrapeWebsite(websiteURL) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(websiteURL);
 
     let content = await page.evaluate(() => {
-        // Source: GamesBeat + GameDeveloper
         let title = Array.from(
                         document.querySelectorAll("h1.article-title"), 
                         item => item.textContent);
@@ -45,11 +55,14 @@ async function scrapeWebsite(websiteURL) {
         };
         return content;
     });
-
+    content.text = filterText(content.text);
     await browser.close();
-
     return content;
 }
 
-export { scrapeWebsite, filterText, printContent };
+module.exports = {
+    scrapeWebsite, 
+    filterText,
+    printContent,
+}
 
