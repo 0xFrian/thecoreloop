259c7c98fdef30153d424c5123a74f0066d1dce9
0xFrian <brianfong22@gmail.com>
2022-07-15 17:05:02 -0700

    :hammer: Added module.exports

--- scripts/rss-feed.js
+++ scripts/rss-feed.js
@@ -46,26 +46,16 @@ function findArticle(filteredList, searchStr) {
 }
 
 // ===== Main function: use GamesBeat RSS =====
-async function parseGamesBeat() {
-    console.log("\nParsing GamesBeat RSS...");
-    let feed = await parser.parseURL("http://feeds.feedburner.com/gamesbeat");
+async function parseRSS(websiteURL) {
+    let feed = await parser.parseURL(websiteURL);
+    console.log(feed.items);
     let filteredList = parseFeed(feed);
-    let searchStr = "metaverse";
-    let article = findArticle(filteredList, searchStr);
-    console.log();
+    return filteredList;
 }
 
-// ===== Main function: use Game Developer RSS =====
-async function parseGameDeveloper() {
-    console.log("\nParsing Game Developer...");
-    let feed = await parser.parseURL("https://www.gamedeveloper.com/rss.xml");
-    let filteredList = parseFeed(feed);
-    let searchStr = "God of War";
-    let article = findArticle(filteredList, searchStr);
-    console.log();
+module.exports = {
+    parseRSS,
+    parseFeed,
+    findArticle,
 }
-
-// ===== Call main functions =====
-parseGamesBeat();
-// parseGameDeveloper();
 
