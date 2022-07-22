a728c78a7989d999b1c0c8a6bbc0fd30005c7e72
0xFrian <brianfong22@gmail.com>
2022-07-22 00:47:47 -0700

    :hammer: Updated routing paths: "/rss-results" and "/web-results"

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Express Server
-const web_parser = require("./scripts/web-scraping.js");
-const rss_parser = require("./scripts/rss-feed.js");
+const web_parser = require("./scripts/parse-web.js");
+const rss_parser = require("./scripts/parse-rss.js");
 const path = require("path");
 const express = require("express");
 const app = express();
@@ -13,31 +13,51 @@ app.set("views", path.join(__dirname, "views"));
 
 // let webURL = "https://venturebeat.com/2022/07/07/red-dead-online-scales-back-support/";
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.post("/", async (req, res) => {
+app.post("/", (req, res) => {
+    res.render("index");
+})
+
+app.post("/rss-results", async (req, res) => {
     let webURL = req.body.webURL;
     if (webURL.length == 0) {
         console.log("Going back to home page...");
         res.render("index");
     } else {
-        console.log(`Obtaining content from ${webURL}...`);
+        console.log(`Fetching web content from ${webURL}...`);
         // Parse web content
-        let content = await web_parser.parseWeb(webURL);
-        web_parser.printContent(content);
-        
-        // Parse RSS content
-        // let content = await rss_parser.parseRSS(webURL);
-        // console.log(content);
+        let resultsFile = "web-results";
+        let data_raw = await web_parser.parseWeb(webURL);
+        let data = web_parser.filterData(data_raw);
 
-        res.render("results", {
+        res.render(resultsFile, {
             webURL: req.body.webURL,
-            content: content,
+            content: data,
         });
     }
-})
+});
+
+app.post("/web-results", async (req, res) => {
+    let webURL = req.body.webURL;
+    if (webURL.length == 0) {
+        console.log("Going back to home page...");
+        res.render("index");
+    } else {
+        console.log(`Fetching RSS feed from ${webURL}...`);
+        // Parse RSS content
+        let resultsFile = "rss-results";
+        let feed = await rss_parser.fetchFeed(webURL);
+        let content = rss_parser.parseFeed(feed);
+
+        res.render(resultsFile, {
+            webURL: req.body.webURL,
+            content: data,
+        });
+    }
+});
 
 const port = 3000;
 app.listen(port, () => {
